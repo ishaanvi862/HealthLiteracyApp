91f8df0aa41cbe1921e90ca40e7924dc87c189b6
ishaanvi862 <ishaanvi.vemula@gmail.com>
2025-07-10 11:52:05 -0500

    Initial Commit

---
+++ iOSApp/iOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAppApp.swift
+//  iOSApp
+//
+//  Created by DPI Student 130 on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSAppTests/iOSAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAppTests.swift
+//  iOSAppTests
+//
+//  Created by DPI Student 130 on 7/10/25.
+//
+
+import Testing
+@testable import iOSApp
+
+struct iOSAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
