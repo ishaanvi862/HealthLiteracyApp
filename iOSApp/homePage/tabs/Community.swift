5bdfb8545b58d9d0c646ba05180e1bf92c0ee5b8
nnolmos1 <nnolmos1@gmail.com>
2025-07-16 12:01:09 -0500

    Community page

--- iOSApp/homePage/tabs/Community.swift
+++ iOSApp/homePage/tabs/Community.swift
@@ -9,11 +9,70 @@ import SwiftUI
 
 struct Community: View {
     var body: some View {
-        Text("Screen 4")
-            .fontWeight(.heavy)
-            .foregroundColor(Color(hue: 0.856, saturation: 1.0, brightness: 1.0))
+        NavigationView {
+            VStack {
+                Text("Hello!")
+                    .fontWeight(.bold)
+                Text("Please allow location services or enter your zip code so we can connect you with the closest resources.") .padding()
+                
+                NavigationLink(destination: CommunityView()) {
+                    
+                    Text("Allow Location")
+                        .padding()
+                        .frame(width: 120)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(12)
+                }
+                .padding()
+                
+                TextField("Enter Zip Code", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .frame(width: 120, alignment: .center)
+                
+                NavigationLink(destination: CommunityView()) {
+                    
+                }
+
+            }
+        }
     }
 }
+
+struct CommunityView: View {
+    var body: some View {
+        
+        Text("Resources Near You:")
+            .bold()
+            .padding()
+            
+        ScrollView {
+            VStack(alignment: .leading) {
+                HStack{
+                    Image("USHealth3")
+                        .resizable()
+                        .frame(width: 150, height: 150)
+                        .cornerRadius(12)
+                    Text("USHealth Advisors Chicago").bold()
+                        + Text("\nLocation: 566 W Adams St #100")
+                        + Text("\nDescription")
+                    
+                }
+                .padding()
+                
+                HStack{
+                    Image("USHealth3")
+                        .resizable()
+                        .frame(width: 150, height: 150)
+                        .cornerRadius(12)
+                    Text("Event Name Here")
+                }
+                .padding()
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+    }
+}
+
 
 #Preview {
     Community()
